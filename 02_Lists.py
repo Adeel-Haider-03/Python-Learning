29ba4d516cb4941bd3f94506ee6ba60de2e594ab
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-04 23:58:58 +0500

    continueList

--- 02_Lists.py
+++ 02_Lists.py
@@ -2,8 +2,8 @@
 
 
 list=[2,3,4,5,6,7,8,9,10]
-list[2:5]=[20,30,40]   #ereplace values from index 2 se 5
-print(list)
+# list[2:5]=[20,30,40]   #replace values from index 2 se 5
+# print(list)
 # print(list[2:5])    #print index 2 se 5 tak
 
 list2=[12,"adeel",True,5.6]
@@ -11,5 +11,13 @@ list2=[12,"adeel",True,5.6]
 # print(type(list2))
 # print(len(list2)) 
 
-list2.insert(2,"hello")   #insert at index 2
-print(list2)
+#list2.insert(2,"hello")   #insert at index 2
+
+#list2.append("world")    #add at last
+#print(list2)
+
+#combineList=list.extend(list2)  #will print none because list.extend(list2) add items of list2 to list at last of list and return nothing
+
+#correctway
+list.extend(list2)
+print(list)   #extend list1 by adding list2 items at last of list1
