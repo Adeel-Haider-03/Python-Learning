d545975e9efdf14837893e4c78837c8fa7e247a2
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-05 23:40:12 +0500

    list_day2

--- 02_Lists.py
+++ 02_Lists.py
@@ -19,5 +19,34 @@ list2=[12,"adeel",True,5.6]
 #combineList=list.extend(list2)  #will print none because list.extend(list2) add items of list2 to list at last of list and return nothing
 
 #correctway
-list.extend(list2)
-print(list)   #extend list1 by adding list2 items at last of list1
+#list.extend(list2)
+#print(list)   #extend list1 by adding list2 items at last of list1
+
+
+#list3=["apple","banana","cherry"]
+# list3.remove("banana")   #remove banana from list3
+
+
+#list3.pop(1)   #remove item at index 1
+#del list3[0]   #remove item at index 0
+
+# list3.clear()   #remove all items from list3
+# print(list3)
+
+# for x in list3:
+#     print(x)
+
+# for i in range(len(list3)):
+#     print(list3[i])
+
+
+#----------------------------------------#list comprehension----------------------------------#
+
+print([x for x in range(10)])   #list comprehension with range {0-10 value print karega}
+
+list4=['adeel','awais','zeeshan','noor','ahmed']
+print([name for name in list4])
+
+
+newlist=[x for x in list4 if 'r' in x]      #will only print noor
+print(newlist)
