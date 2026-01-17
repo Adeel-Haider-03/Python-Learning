80db6e8b867aff307ea4e0ee5836808837ea61b7
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-17 22:10:45 +0500

    range

---
+++ 08_Range.py
@@ -0,0 +1,17 @@
+#The range() function can be called with 1, 2, or 3 arguments, using this syntax:
+
+#range(start, stop, step)
+
+# x=range(10,100,5)
+# print(x)   #it will print the range object
+
+# # we need ro cinvert to list or either loop through it to see the values
+
+# print(list(x))  # it will print the values in the range object
+
+# for i in x:
+#     print(i)
+
+
+for x in range(0,20,4):
+    print(x)
