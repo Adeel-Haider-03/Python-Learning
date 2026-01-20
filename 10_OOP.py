062d6adafa37eb70ea4084ff23f5a4688e3fe075
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-20 23:16:16 +0500

    oopStart

---
+++ 10_OOP.py
@@ -0,0 +1,8 @@
+class Car:
+    modelName = "Mustang"
+    year = 1964
+    color = "Red"
+
+
+myCar = Car()
+print("My car is a " + myCar.color + " " + myCar.modelName + " from " + str(myCar.year) + ".")
