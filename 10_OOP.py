c2f0356caf5442fd7ed1b43853ac2293fb17b6b1
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-22 23:01:36 +0500

    class2

--- 10_OOP.py
+++ 10_OOP.py
@@ -1,8 +1,17 @@
-class Car:
-    modelName = "Mustang"
-    year = 1964
-    color = "Red"
+class Animal:
+    def __init__(self,name,sound):
+        self.name=name
+        self.sound=sound
 
+    def describe(self):    #self is neccessary to  put as first parameter in method
+        return f'{self.name} makes {self.sound} sound'
 
-myCar = Car()
-print("My car is a " + myCar.color + " " + myCar.modelName + " from " + str(myCar.year) + ".")
+Animal1=Animal("cow","mooo")
+
+Animal1.color="white"
+
+print(Animal1.name)
+print(Animal1.sound)
+print(Animal1.color)
+
+print(Animal1.describe())
