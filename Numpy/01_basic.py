bd15e8de4d3abdbb2d7db2cb71cd8386e4509954
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-27 22:16:41 +0500

    dimemsionalArray

--- Numpy/01_basic.py
+++ Numpy/01_basic.py
@@ -1,5 +1,15 @@
 import numpy as np
 
-arr=np.array([1,2,3,4,5])
-print(arr)
-print(type(arr))
+# arr=np.array([1,2,3,4,5])
+# print(arr)
+# print(type(arr))
+
+# multi-dimensial array
+
+arr2=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+print(arr2)
+print(type(arr2))
+print((arr2.ndim))    
+print((arr2.shape))  
+
+print(arr2[0,1,1])
