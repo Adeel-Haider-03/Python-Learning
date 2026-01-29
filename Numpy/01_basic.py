ad885b0fdca59fe99754131b0fda6794f3f36d94
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-29 17:38:16 +0500

    conversion

--- Numpy/01_basic.py
+++ Numpy/01_basic.py
@@ -6,11 +6,20 @@ import numpy as np
 
 # multi-dimensial array
 
-arr2=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
-print(arr2)
-print(type(arr2))
-print((arr2.ndim))    
-print((arr2.shape))  
+# arr2=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+# print(arr2)
+# print(type(arr2))
+# print((arr2.ndim))    
+# print((arr2.shape))  
 
-print(arr2[0,1,1])
-print(arr2[0,2,3])
+# print(arr2[0,1,1])
+# # print(arr2[0,2,3])
+
+arr3=np.array([1,2,3,4],dtype='f') #create array with specific data type
+print(arr3)
+print(arr3.dtype)
+
+#convert data type
+arr3Convert=arr3.astype('i')
+print(arr3Convert)
+print(arr3Convert.dtype)
