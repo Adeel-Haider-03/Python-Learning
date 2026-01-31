e80e4e692b6d9172720a7ce85d5a93c8dce6a822
Adeel Haider <adeelhaider3360@gmail.com>
2026-01-31 21:42:44 +0500

    Joining

--- Numpy/02_methods.py
+++ Numpy/02_methods.py
@@ -12,7 +12,35 @@ arr=np.array([1,2,3,4,5])
 # print(x)    #x remains unchanged
 
 #view
-y=arr.view()  #create a view of arr
-arr[1]=88
-print(arr)  #modified arr
-print(y)    #y is also modified since it's a view of arr
+# y=arr.view()  #create a view of arr
+# arr[1]=88
+# print(arr)  #modified arr
+# print(y)    #y is also modified since it's a view of arr
+
+
+#concatenate
+
+# arr1=np.array([1,2,3])
+# arr2=np.array([4,5,6])
+# # concateArr=np.concatenate((arr1,arr2))
+# concateArr=np.concatenate((arr1,arr2),axis=1)
+# print(concateArr)
+
+# arr1=np.array([[1,2],[3,4]])
+# arr2=np.array([[5,6],[7,8]])
+# # concateArr=np.concatenate((arr1,arr2))  #default axis=0
+# concateArr=np.concatenate((arr1,arr2),axis=1)  #concatenate along columns
+# print(concateArr)
+
+
+
+#stack
+arr1=np.array([1,2,3,4])
+arr2=np.array([5,6,7,8])
+# concatArr=np.stack((arr1,arr2)) 
+# concatArr=np.vstack((arr1,arr2)) #vertical stack
+# concatArr=np.hstack((arr1,arr2)) #horizontal stack
+concatArr=np.dstack((arr1,arr2)) #depth stack
+
+print(concatArr)
+
