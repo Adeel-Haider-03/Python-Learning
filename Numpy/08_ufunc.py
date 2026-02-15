578af841615145cac2bfd29d04bcb94b740000f0
Adeel Haider <adeelhaider3360@gmail.com>
2026-02-15 23:56:01 +0500

    ufunc

---
+++ Numpy/08_ufunc.py
@@ -0,0 +1,12 @@
+#ufuncs is a function that operates on the ndarray elementwise. It is a fast and efficient way to perform operations on arrays.
+
+import numpy as np
+a=np.array([1,2,3,4])
+b=np.array([5,6,7,8])
+#add
+x=np.add(a,b)
+print(x)
+
+#subtract
+# y=np.subtract(a,b)
+# print(y)
