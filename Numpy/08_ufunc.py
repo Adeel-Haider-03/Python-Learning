814097e10cfe9e33a62f71c5aa6ef6a3492ce672
Adeel Haider <adeelhaider3360@gmail.com>
2026-02-16 23:05:46 +0500

    ufunc_etc

--- Numpy/08_ufunc.py
+++ Numpy/08_ufunc.py
@@ -18,3 +18,51 @@ c=[]
 for i,j in zip(a,b):
     c.append(i+j)
 print(c)
+
+
+#let's create a custom ufunc to perform elementwise addition
+def myAdd(x,y):
+    return x+y
+
+#we can use np.frompyfunc to create a ufunc from a python function
+customAdd=np.frompyfunc(myAdd,2,1)  #(func name, no of arguments, no of returns)
+
+y=customAdd(a,b)
+print(y)
+
+#we can also use the built-in ufuncs to perform elementwise operations on arrays
+
+arr1=np.array([1,2,3,4])
+arr2=np.array([5,6,7,8])
+
+plus= np.add(arr1,arr2)
+print(plus)
+
+minus=np.subtract(arr1,arr2)
+print(minus)
+
+div=np.divide(arr1,arr2)
+print(div)
+
+mod=np.mod(arr1,arr2)
+print(mod)
+
+#The divmod() function return both the quotient and the mod. The return value is two arrays,
+#  the first array contains the quotient and second array contains the mod.
+divandmod=np.divmod(arr1,arr2)
+print(divandmod)
+
+arr3=np.array([-1,-4,-5])
+absol=np.absolute(arr3)
+print(absol)
+
+
+# it has many more ufunc
+#  for sum, commulative sum,
+#  product,
+#  log,
+#  LCM, GCD,
+#  sin cos,
+#  hyperbolic,
+#  set operations
+#  and many others
