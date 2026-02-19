f3f7aeb237b36100f8d4f540a29b821480b2ec62
Adeel Haider <adeelhaider3360@gmail.com>
2026-02-19 23:58:46 +0500

    changingvalue

--- Pandas/03_cleaningData.py
+++ Pandas/03_cleaningData.py
@@ -14,6 +14,18 @@ data=pd.read_csv('data.csv')
 # print(data.info())
 
 # print(data['Calories'].to_string())
-data['Calories'].replace('NaN',0, inplace=True)  #to fill missing values with 0
-# data['Calories'].fillna(data['Calories'].mean(), inplace=True)  #to fill missing values with the mean of the column
-print(data['Calories'].to_string())
+# data['Calories'].replace('NaN',0, inplace=True)  #to fill missing values with 0
+# # data['Calories'].fillna(data['Calories'].mean(), inplace=True)  #to fill missing values with the mean of the column
+# print(data['Calories'].to_string())
+# print(data['Duration'].to_string())
+
+#replace the value at index 69 in the 'Duration' column with 45
+# data.at[69,'Duration']=45
+
+# data.loc[69,'Duration']=50
+
+#why .loc doesn't work here? because .loc is used for label-based indexing,
+# and the index 69 is not a label but a positional index. To use .loc, you would need to set the index of the DataFrame to a column that contains unique labels, or use .iloc for positional indexing.
+
+data.iloc[69, data.columns.get_loc('Duration')]=50  #to replace the value at index 69 in the 'Duration' column with 45
+print(data['Duration'].to_string())
