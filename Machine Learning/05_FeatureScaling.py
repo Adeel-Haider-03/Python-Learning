b3a94c3efc1cf5ad4b536fdea9284583d332b73a
Adeel Haider <adeelhaider3360@gmail.com>
2026-02-26 00:14:51 +0500

    scaling2

--- Machine Learning/05_FeatureScaling.py
+++ Machine Learning/05_FeatureScaling.py
@@ -6,8 +6,31 @@ scale=StandardScaler() #create an instance of the StandardScaler class
 
 df=pd.read_csv('data.csv')
 
+#convert Volume into litres
+df['Volume']=df['Volume']/1000
+
+
+# print("Before scaling:")
+# print(df)
+
+#now the Volume and weight have different scales, we need to scale them before fitting the model
+
 X=df[['Volume', 'Weight']]  #independent variables
 
-print("Before scaling:")
-print(X)
+scaled_x=scale.fit_transform(X) #fit the scaler to the data and transform it
 
+# print("After scaling:")
+# print(scaled_x)
+
+
+Y=df[['CO2']] 
+
+regr=linear_model.LinearRegression() #create a linear regression model
+regr.fit(scaled_x,Y) #fit the model to the scaled data
+
+#when we scale features we have to use the same scaler to transform the new data before making predictions
+prediction=scale.transform([[2.3, 1300]]) #scale the new data using the same scaler
+print("Scaled prediction input:", prediction)
+
+predicted_co2=regr.predict([prediction[0]]) 
+print("Predicted CO2:", predicted_co2)
